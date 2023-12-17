ea0f94da3fc7cde493767a2e66fd193b213872e5
Jakub Kościołowski <kj36081>
2023-12-17 21:33:29 +0100

    lab-c

---
+++ lab-c/src/app/person.ts
@@ -0,0 +1,10 @@
+export interface Person {
+    firstName?: string;
+    lastName?: string;
+    age?: string;
+    address: {
+        city?: string;
+        street?: string;
+        postCode?: string
+    }
+}
